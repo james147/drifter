d10f4fa4788ed4c31366a8a5947338153c7914d7
Michael Daffin <james1479@gmail.com>
2014-03-13 19:25:37 +0000

    Updates the main sketch to test the libraries
    Just some basic tests to test the some of the logic in the libraries to
    make sure it does what it should do.

--- src/drifter.ino
+++ src/drifter.ino
@@ -1,22 +1,43 @@
 #include <Motor.h>
 #include <Button.h>
+#include <LED.h>
 
-Button switch1;
-Button switch2;
+Button switch1(4, PULLUP);
+Button switch2(8, PULLUP);
+LED led1(9);  // PWM pin
+LED led2(13); // Non PWM pin
 Motor left_motor;
 Motor right_motor;
 
+bool running = false;
+int speed = -1;
+
 void setup()
 {
-    switch1.attach(4);
-    switch2.attach(8);
+    Serial.begin(9600);
     left_motor.attach(10, 11, 12);
     right_motor.attach(5, 6, 7);
 }
 
 void loop()
 {
-    int speed = 1;
-    left_motor.forward(speed);
-    right_motor.forward(speed);
+    // Stop if the user has pressed the second switch
+    if (switch2.uniquePress()) {
+        running = false;
+        speed = -1;
+        left_motor.stop();
+        right_motor.stop();
+        return;
+    }
+    
+    // Incroment speed if the first switch is pressed
+    if (switch1.uniquePress()) {
+        speed++;
+        if (speed > 10) {
+            speed = 0;
+        }
+        left_motor.forward(speed);
+        right_motor.forward(speed);
+    }
+    
 }
