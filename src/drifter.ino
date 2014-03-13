4e3ea659dec53f4921ebd1e2d540fd4c494e14db
Michael Daffin <james1479@gmail.com>
2014-03-13 18:29:04 +0000

    Switches to the offical Button and LDE lib
    Repalces the old Bounce2 with the offical Button and LED lib

--- src/drifter.ino
+++ src/drifter.ino
@@ -1,8 +1,22 @@
+#include <Motor.h>
+#include <Button.h>
+
+Button switch1;
+Button switch2;
+Motor left_motor;
+Motor right_motor;
 
 void setup()
 {
+    switch1.attach(4);
+    switch2.attach(8);
+    left_motor.attach(10, 11, 12);
+    right_motor.attach(5, 6, 7);
 }
 
 void loop()
 {
+    int speed = 1;
+    left_motor.forward(speed);
+    right_motor.forward(speed);
 }
