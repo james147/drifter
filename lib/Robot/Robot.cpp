964c66e79363d741ee67409d81852194fb66c7df
Michael Daffin <james1479@gmail.com>
2014-03-15 18:31:38 +0000

    Adds general robot function to a robot library
    This library will be used to control the robot and keep all its
    functions in one place. It will contains methods for minipulating all of
    the hardware attached to the robot, but no actual contol logic.

---
+++ lib/Robot/Robot.cpp
@@ -0,0 +1,56 @@
+#include <Robot.h>
+
+Robot::Robot() : led(13), button(A0, PULLUP), _running(false), _speed(1), _state(OFF) {
+}
+
+void Robot::init() {
+    _left_motor.attach(9, 8, 7);
+    _right_motor.attach(6, 5, 4);
+    loadSettings();
+}
+
+void Robot::loadSettings() {
+    byte left_min = EEPROM.read(LEFT_MIN_POWER_ADDRESS);
+    byte right_min = EEPROM.read(RIGHT_MIN_POWER_ADDRESS);
+
+    if (right_min > 100 || left_min > 100) {
+        left_min = 50;
+        right_min = 50;
+    } else {
+        _left_motor.setMinPower(left_min);
+        _right_motor.setMinPower(right_min);
+    }
+}
+
+void Robot::saveSettings() {
+}
+
+void Robot::forward(int speed) {
+    _speed = speed;
+    _state = FORWARD;
+    _left_motor.forward(speed);
+    _right_motor.forward(speed);
+}
+
+void Robot::reverse(int speed) {
+    _speed = speed;
+    _state = REVERSE;
+    _left_motor.reverse(speed);
+    _right_motor.reverse(speed);
+}
+
+void Robot::stop() {
+    _left_motor.stop();
+    _right_motor.stop();
+    _state = STOPPED;
+}
+
+void Robot::off() {
+    _left_motor.off();
+    _right_motor.off();
+    _state = OFF;
+}
+
+bool Robot::isRunning() {
+    return _state != STOPPED && _state != OFF;
+}
---
+++ lib/Robot/Robot.h
@@ -0,0 +1,49 @@
+#include <Arduino.h>
+#include <Motor.h>
+#include <Button.h>
+#include <LED.h>
+#include <EEPROM.h>
+
+#ifndef ROBOT_H
+#define ROBOT_H
+
+class Robot
+{
+    private:
+        static const int LEFT_MIN_POWER_ADDRESS = 0;
+        static const int RIGHT_MIN_POWER_ADDRESS = 1;
+
+    public:
+        enum State {
+            FORWARD, 
+            REVERSE,
+            STOPPED,
+            OFF,
+            ROTATING_LEFT,
+            ROTATING_RIGHT
+        };
+
+        Robot();
+        void init();
+        void loadSettings();
+        void saveSettings();
+        void forward(int speed);
+        void reverse(int speed);
+        void stop();
+        void off();
+        
+        bool isRunning();
+
+        Button button;
+        LED led;
+
+    private:
+        Motor _left_motor;
+        Motor _right_motor;
+
+        bool _running;
+        int _speed;
+        State _state;
+};
+
+#endif // ROBOT_H
