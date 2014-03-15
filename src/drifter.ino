878634ad3f31563433bdc0b3eeb9821af9ce531c
Michael Daffin <james1479@gmail.com>
2014-03-15 18:33:32 +0000

    Updates the main sketch to use the new robot library

--- src/drifter.ino
+++ src/drifter.ino
@@ -3,53 +3,27 @@
 #include <Button.h>
 #include <LED.h>
 #include <EEPROM.h>
+#include <Robot.h>
 
-Button button(A0, PULLUP);
-LED led(13);
-Motor left_motor;
-Motor right_motor;
-static const int LEFT_MIN_POWER_ADDRESS = 0;
-static const int RIGHT_MIN_POWER_ADDRESS = 1;
+Robot robot;
 
-bool running = false;
-int speed = 5;
-
-void setup()
-{
+void setup() {
     Serial.begin(9600);
-    left_motor.attach(9, 8, 7);
-    right_motor.attach(6, 5, 4);
-    Serial.println("Loading EEPROM values");
-    byte left_min = EEPROM.read(LEFT_MIN_POWER_ADDRESS);
-    byte right_min = EEPROM.read(RIGHT_MIN_POWER_ADDRESS);
-
-    if (right_min > 100 || left_min > 100) {
-        Serial.println("Values uninitlised, initlising");
-        left_min = 50;
-        right_min = 50;
-    } else {
-        left_motor.setMinPower(left_min);
-        right_motor.setMinPower(right_min);
-    }
-    Serial.print("Left motor min power: ");
-    Serial.println(left_min);
-    Serial.print("Right motor min power: ");
-    Serial.println(right_min);
-    Serial.println("Started");
+    robot.init();
 }
 
-void loop()
-{
-    if (button.uniquePress()) {
-        if (running) {
-            left_motor.stop();
-            right_motor.stop();
-            running = false;
+void loop() {
+    if (robot.button.uniquePress()) {
+        if (robot.isRunning()) {
+            robot.stop();
         } else {
-            left_motor.forward(speed);
-            right_motor.forward(speed);
-            running = true;
+            robot.forward(5);
         }
     }
 }
 
+String command = "";
+
+void serialEvent() {
+    
+}
